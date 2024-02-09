2de5e999aa41c29ceb17d28f37d05dffc70f9482
Tassio Gomes <ds.tassio@gmail.com>
2024-02-09 15:43:31 +0000

    2.2.14 feito o foreach na tabela de tipoocorrencia.html

--- Projeto_Final/TassioWeb2/src/app/tipoocorrencia/tipoocorrencia.component.ts
+++ Projeto_Final/TassioWeb2/src/app/tipoocorrencia/tipoocorrencia.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TipoOcorrenciaInterface } from './tipoocorrencia';
 import { TipoocorrenciaService } from './tipoocorrencia.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tipoocorrencia',
   templateUrl: './tipoocorrencia.component.html',
   styleUrl: './tipoocorrencia.component.css'
 })
-export class TipoocorrenciaComponent {
-  funcoesList: TipoOcorrenciaInterface[] = [];
-  funcao?: TipoOcorrenciaInterface = undefined;
+export class TipoocorrenciaComponent implements OnInit {
+  tipoocorrenciaList: TipoOcorrenciaInterface[] = [];
+  tipoocorrencia?: TipoOcorrenciaInterface = undefined;
 
   constructor (private TipoocorrenciaService: TipoocorrenciaService){
-    
+
+  }
+  ngOnInit(): void {
+    this.TipoocorrenciaService.getAllTipoOcorrencia().then((data: Observable<TipoOcorrenciaInterface[]>) => {
+      data.subscribe(list => {
+        console.log(list);
+        this.tipoocorrenciaList = list;
+      });
+
+    }).catch(error => {
+      console.log(error);
+    })
+
+
+    throw new Error('Method not implemented.');
   }
 
 
